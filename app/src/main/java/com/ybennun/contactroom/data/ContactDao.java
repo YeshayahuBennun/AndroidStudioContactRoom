59be8e365af83bb9315afca830bef33e04900bfc
YeshayahuBennun <ybennun@yahoo.com>
2022-02-14 19:11:13 +0200

    commit#3 - Contact Room - Setting Up Android ROOM DAO & LiveData

---
+++ app/src/main/java/com/ybennun/contactroom/data/ContactDao.java
@@ -0,0 +1,25 @@
+package com.ybennun.contactroom.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.ybennun.contactroom.model.Contact;
+
+import java.util.List;
+
+@Dao
+public interface ContactDao {
+    //CRUD
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Contact contact);
+
+    @Query("DELETE FROM contact_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM contact_table")
+    LiveData<List<Contact>> getAllContacts();
+
+}
