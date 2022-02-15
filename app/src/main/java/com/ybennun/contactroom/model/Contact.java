5a88902f5c313441465abe15c74b113ea04302c4
YeshayahuBennun <ybennun@yahoo.com>
2022-02-15 19:08:21 +0200

    commit#4 - Contact Room -  RoomDatabase Singleton Class

--- app/src/main/java/com/ybennun/contactroom/model/Contact.java
+++ app/src/main/java/com/ybennun/contactroom/model/Contact.java
@@ -28,11 +28,23 @@ public class Contact {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getOccupation() {
         return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
     }
 }
